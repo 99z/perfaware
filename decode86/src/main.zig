197f0f9a23d439f9b09031ee4c53ac1c45f0bdf9
Nicolas S. Montanaro <7397825+99z@users.noreply.github.com>
2025-04-30 12:10:32 -0400

    ref decoder building

--- decode86/src/main.zig
+++ decode86/src/main.zig
@@ -2,6 +2,26 @@ const std = @import("std");
 const sim86 = @import("sim86.zig");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("sim86 version: {}\n", .{sim86.getVersion()});
+    std.debug.print("sim86 reference version: {}\n", .{sim86.getVersion()});
+
+    var args = std.process.args();
+    _ = args.next();
+	// const exec = std.mem.eql(u8, args.next() orelse "", "-exec");
+
+    const filename = args.next() orelse return std.debug.print("usage: decode86 <BIN_FILENAME>\n", .{});
+    const file = try std.fs.cwd().openFile(filename, .{});
+    defer file.close();
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+	defer {
+		_ = gpa.deinit();
+	}
+
+    const stat = try file.stat();
+    const buffer = try file.readToEndAlloc(allocator, stat.size);
+    defer allocator.free(buffer);
+
+	const decoded = sim86.decode8086Instruction(buffer);
+	std.debug.print("{any}\n", .{decoded});
 }
